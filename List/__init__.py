0e8eb069234798864ea91801f5c945dba353290d
Nassar <nassarragi@gmail.com>
2024-03-23 17:27:53 +0000

    Added list function

---
+++ List/__init__.py
@@ -0,0 +1,29 @@
+import logging
+import azure.functions as func
+import json
+from azure.cosmos import CosmosClient, PartitionKey, exceptions
+import os
+
+endpoint = os.environ['COSMOS_ENDPOINT']
+credential = os.environ['COSMOS_KEY']
+client = CosmosClient(endpoint, credential=credential)
+database_name = 'url-shortener'
+database = client.get_database_client(database_name)
+container_name = 'urls'
+container = database.get_container_client(container_name)
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('List function processed a request.')
+    items = list(container.query_items(query='SELECT * FROM c', enable_cross_partition_query=True))
+    base_url = "http://localhost:7071/api"
+    list = [
+        {
+            "Shortened URL": f"{base_url}/{item.get('id')}",
+            "Long URL": item.get["received_url"],
+            "Description": item.get["received_description", "No description found"]
+        }
+        for item in items
+        
+    ]
+    
+    return func.HttpResponse(json.dumps(list), status_code=200, headers={"Content-Type": "application/json"})
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import os
 import json
 
-def main():
+def create_shortcode():
     url = input("Enter a URL: ")
     short_code = input("Enter a short code: ")
     description = input("Enter a description(optional): ")
@@ -13,17 +13,20 @@ def main():
         "description": description if description else ""
     }
     
-    function_url = os.environ.get("FUNCTION_URL")
+    function_url = os.environ.get("CREATE_FUNCTION_URL")
     
     response = requests.post(function_url, json=data)
-    print(f"Status Code: {response.status_code}")
-    print(f"Function URL: {function_url}")
+    # Debugging
+    # print(f"Status Code: {response.status_code}")
+    # print(f"Function URL: {function_url}")
     try:
         response_data = response.json()
         print(f"Short URL: {response_data.get('short_url', 'No short URL found')}")
     except json.JSONDecodeError:
         print("Error creating short URL")
 
-if __name__ == "__main__":
-    main()
+
+
+# if __name__ == "__main__":
+#   main()
     
